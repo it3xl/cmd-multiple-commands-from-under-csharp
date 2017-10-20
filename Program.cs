0a8ccd2fcbb95dcb1005c5aa5cf3f2b6af72fcab
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2017-10-20 18:57:07 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics;
+
+namespace PrototypingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo info = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+
+                // The Process object must have the UseShellExecute property set to false in order to redirect IO streams.
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+
+            var proc = new Process();
+            // The "using" is more safe alternative for "proc.Close()" to release resources in the process' wrapper.
+            using (proc)
+            {
+                proc.StartInfo = info;
+                proc.Start();
+
+                proc.StandardInput.WriteLine(@"""C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat""");
+                proc.StandardInput.WriteLine();
+                proc.StandardInput.WriteLine("not_existing_command /oops");
+                proc.StandardInput.WriteLine();
+                proc.StandardInput.WriteLine(@"ECHO /?");
+                proc.StandardInput.WriteLine();
+                proc.StandardInput.WriteLine("ECHO ErrorLever = %ERRORLEVEL%");
+
+                // Allow not-blocking use of ReadToEnd().
+                // User the CMD EXIT command vs "proc.StandardInput.Close()" to pass the exit code to .NET proc.ExitCode below;
+                proc.StandardInput.WriteLine("EXIT");
+                // No more the used CMD process here.
+
+                var output = proc.StandardOutput.ReadToEnd();
+                var errorOutput = proc.StandardError.ReadToEnd();
+
+                var exitCode = proc.ExitCode;
+                if(exitCode != 0)
+                {
+                    // Your actions.
+                }
+            }
+
+        }
+
+　
+    }
+}
