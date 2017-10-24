5adbe282fac068b846b4ad2c396b4c718aaf275f
Ilya Tretyakov #it3xl <it3xl@users.noreply.github.com>
2017-10-24 13:15:11 +0300

    Moved to projects. Added a wrapper for an encapsulation.

---
+++ CmdShellProj/CmdShell.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CmdShellProj
+{
+    /// <summary>
+    /// it3xl/CMD-shell_from_under_csharp
+    /// https://github.com/it3xl/CMD-shell_from_under_csharp/blob/master/Program.cs
+    /// </summary>
+    public class CmdShell
+    {
+        public void Execute(string cmdCommands)
+        {
+            var commandsList = cmdCommands.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var info = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+
+                // The Process object must have the UseShellExecute property set to false in order to redirect IO streams.
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+
+            var proc = new Process();
+            // The "using" is more safe alternative for "proc.Close()" to release resources.
+            using (proc)
+            {
+                proc.StartInfo = info;
+                proc.Start();
+
+                // This could be useful for somebody to use async reads if you wish.
+                //proc.BeginOutputReadLine
+                //proc.BeginErrorReadLine
+
+                commandsList.ToList()
+                    .ForEach(command => proc
+                        .StandardInput.WriteLine(command));
+
+                // Allow not-blocking use of ReadToEnd().
+                // Use the CMD EXIT command vs "proc.StandardInput.Close()" to pass the exit code to .NET proc.ExitCode below;
+                proc.StandardInput.WriteLine("EXIT");
+                // At this point, the used CMD process does not exist anymore.
+
+                var waitSeconds = 600;
+                var interrupted = !proc.WaitForExit(waitSeconds * 1000);
+
+                if (interrupted)
+                {
+                    //throw new Exception(string.Format("Was interrupted after waiting for {0} seconds.", waitSeconds));
+                }
+
+                var output = proc.StandardOutput.ReadToEnd();
+                var errorOutput = proc.StandardError.ReadToEnd();
+
+                var exitCode = proc.ExitCode;
+                if (exitCode != 0)
+                {
+                    // STUB: Remove the return.
+                    return;
+
+                    throw new Exception(string.Format(@"Error exit code {0} received.
+Error Output:
+{1}
+
+Output:
+{2}
+",
+                        exitCode,
+                        errorOutput,
+                        output
+                        ));
+                }
+            }
+
+        }
+    }
+}
---
+++ ConsoleRunner/Program.cs
@@ -0,0 +1,36 @@
+﻿using CmdShellProj;
+
+namespace ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msBuildCommandPrompt = @"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsMSBuildCmd.bat";
+            var somePath = @"C:\temp";
+
+            new CmdShell()
+                .Execute(string.Format(@"
+none_existing_command /oops
+ping example.com -n 5
+none_existing_command /oops
+
+""{0}""
+
+CD ""{1}""
+
+MsBuild SomeProject.csproj^
+ /target:Build^
+ /p:Configuration=Release^
+ /verbosity:normal^
+ /maxCpuCount
+
+ECHO ErrorLever = %ERRORLEVEL%
+
+",
+                msBuildCommandPrompt,
+                somePath
+                ));
+        }
+    }
+}
